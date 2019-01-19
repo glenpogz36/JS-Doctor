0320518d3d141a1078b5a11fe95131e4a36bb641
glen <glen_sale@yahoo.com>
2019-01-18 17:44:33 -0800

    it will add more bios

--- src/main.js
+++ src/main.js
@@ -22,8 +22,36 @@ $(document).ready(function () {
                 $("#results").text("No doctor found, try another one.");
             }
             for (var i = 0; i < result.length; i++) {
-                $("#results").append("<h4>" + result[i].profile.first_name + " " + result[i].profile.last_name);
+                $("#results").append("<h4>" + result[i].profile.first_name + " " + result[i].profile.last_name + "</h4>" + "Phone number: " + result[i].practices[0].phones[0].number + "<br>" + "<br>" + "<h5>" + "Address : " + "</h5>" + "City: " + result[i].practices[0].visit_address.city + "<br>" + "State: " + result[i].practices[0].visit_address.state + "<br>" +
+                    "Street: " + result[i].practices[0].visit_address.street + "<br>" +
+                    "Zip: " + result[i].practices[0].visit_address.zip + "<br>" +
+                    "Available: " + result[i].practices[0].accepts_new_patients + "<br>" + "<hr>");
             }
         });
     });
+    //
+    $("#illnessForm").submit(function (event) {
+        event.preventDefault();
+        $("#results").text("");
+        $("#error").text("");
+        let illness = $("#illnessSearch").val();
+        let newSearch = new Search();
+        let promise = newSearch.foundIllness(illness);
+
+        promise.then(function (response) {
+            let body = JSON.parse(response);
+            let result = body.data;
+            if (body.data.length === 0) {
+                $("#error").text("Results not found.");
+            }
+            for (var i = 0; i < result.length; i++) {
+                $("#results").append("<h4>" + result[i].profile.first_name + " " + result[i].profile.last_name + "</h4>" + "Phone number: " + result[i].practices[0].phones[0].number + "<br>" + "Address: " + "City: " + result[i].practices[0].visit_address.city + "<br>" + "State: " + result[i].practices[0].visit_address.state + "<br>" +
+                    "Street: " + result[i].practices[0].visit_address.street + "<br>" +
+                    "Zip: " + result[i].practices[0].visit_address.zip + "<br>" +
+                    "Available: " + result[i].practices[0].accepts_new_patients + "<br>" +
+                    "Bio: " + result[i].profile.bio + "<br>" + "<hr>");
+            }
+        })
+    })
+
 });
