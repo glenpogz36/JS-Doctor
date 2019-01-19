23539d32d5616e88822820ae10d2100c6f814a40
glen <glen_sale@yahoo.com>
2019-01-18 16:16:41 -0800

    it will add backend logic to grab doctor API using firstname and last name

--- src/main.js
+++ src/main.js
@@ -2,14 +2,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './styles.css';
 import $ from 'jQuery';
+import { Search } from "./doctor.js";
 
 $(document).ready(function () {
-    $('form#doctor').submit(function (event) {
+    $("#doctorForm").submit(function (event) {
         event.preventDefault();
-        let name = $('#doctorName').val();
-        let condition = $('#condition').val();
-        $('#doctorName').val("");
-        $('#condition').val("");
-        $("#results").append(name, condition);
+        $("#results").text("");
+        $("#error").text("");
+        let doctor = $("#doctorSearch").val();
+        let newSearch = new Search();
+        let promise = newSearch.foundDoctor(doctor);
+
+        promise.then(function (response) {
+            let body = JSON.parse(response);
+            let result = body.data;
+            console.log(result);
+
+            if (body.data.length === 0) {
+                $("#results").text("No doctor found, try another one.");
+            }
+            for (var i = 0; i < result.length; i++) {
+                $("#results").append("<h4>" + result[i].profile.first_name + " " + result[i].profile.last_name);
+            }
+        });
     });
 });
